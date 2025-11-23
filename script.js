f6531693e10a46d791c94ea356470cfc21302cdd
NannaNinus10 <nannaskovdagsvad@gmail.com>
2025-11-23 13:07:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-// TESTDATO (fjern/kommentér denne linje når det er "rigtigt")
-const testDate = new Date("2025-12-24");
-
 function getToday() {
-  return testDate ? testDate : new Date();
+  return new Date();
 }
 
-// Tilføj klik på hver låge
 document.querySelectorAll(".door").forEach(door => {
   door.addEventListener("click", () => {
     const day = parseInt(door.getAttribute("data-day"));
@@ -13,7 +9,6 @@ document.querySelectorAll(".door").forEach(door => {
     const month = getToday().getMonth() + 1;
 
     if (month === 12 && today >= day) {
-      // toggle låge åben/lukket
       if (door.classList.contains("open")) {
         door.classList.remove("open");
         door.classList.add("closed");
@@ -26,13 +21,3 @@ document.querySelectorAll(".door").forEach(door => {
     }
   });
 });
-
-
-
-
-
-
-
-
-
-
